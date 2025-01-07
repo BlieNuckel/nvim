1818cb8994be73861fe2171ad416e4496ef2c730
Lasse Poulsen <work.lasse.face@gmail.com>
2025-01-07 19:33:13 +0100

    Add tooltip keybind

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -6,5 +6,5 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
-
+map("n", "gh", vim.lsp.buf.hover, { desc = "LSP Tooltip" })
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")

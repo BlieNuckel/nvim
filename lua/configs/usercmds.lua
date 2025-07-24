23bb5a839407a5e52e535499acb1ed12c2509929
Lasse Poulsen <lasse.poulsen@pinmeto.com>
2025-07-24 12:05:21 +0200

    Add usercmd to close all buffers except current

--- lua/configs/usercmds.lua
+++ lua/configs/usercmds.lua
@@ -1,6 +1,11 @@
 local usercmd = vim.api.nvim_create_user_command
 
 usercmd("BufCloseAll", function()
-  vim.cmd "%bd"
+  vim.cmd "%bd!"
+  vim.cmd "NvimTreeOpen"
+end, {})
+
+usercmd("BufCloseOthers", function()
+  vim.cmd "%bd!|e#"
   vim.cmd "NvimTreeOpen"
 end, {})

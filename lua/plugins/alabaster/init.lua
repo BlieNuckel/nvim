7c6d710efab2e57b51e687468bd13f25013292ba
Lasse Poulsen <lassekrp@pm.me>
2025-10-24 22:41:37 +0200

    remove alabaster theme

--- lua/plugins/alabaster/init.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "p00f/alabaster.nvim",
-  lazy = false, -- make sure we load this during startup
-  priority = 1000, -- load before other plugins
-  config = function()
-    vim.cmd.colorscheme "alabaster"
-  end,
-}

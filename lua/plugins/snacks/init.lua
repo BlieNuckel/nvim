716afc0b11406ffec03731ee53bcb9a6d518a307
Lasse Poulsen <lasse.poulsen@pinmeto.com>
2025-11-03 13:35:04 +0100

    fix: snacks not enabled

--- lua/plugins/snacks/init.lua
+++ lua/plugins/snacks/init.lua
@@ -2,4 +2,9 @@ return {
   "folke/snacks.nvim",
   priority = 1000,
   lazy = false,
+  opts = {
+    input = { enabled = true },
+    scroll = { enabled = true },
+    notifier = { enabled = true },
+  },
 }

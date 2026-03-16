c54da9e1e74ba92a6d0ffa97a5a4ad7db0c27b1e
Lasse Poulsen <lasse.poulsen@pinmeto.com>
2026-03-16 07:46:48 +0100

    feat: render markdown plugin

---
+++ lua/plugins/rendermarkdown/init.lua
@@ -0,0 +1,8 @@
+return {
+  "MeanderingProgrammer/render-markdown.nvim",
+  cmd = { "RenderMarkdown" },
+  dependencies = { "nvim-mini/mini.icons" }, -- if you use the mini.nvim suite
+  ---@module 'render-markdown'
+  ---@type render.md.UserConfig
+  opts = {},
+}
